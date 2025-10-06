8ab7729fde53aaf1a059f02c3c85ced8822f1024
errphoenix <verycoolguy76@gmail.com>
2025-10-06 22:32:38 +0200

    chore(crsh-gui): remove redundant dependencies

--- crsh-gui/src-tauri/src/lib.rs
+++ crsh-gui/src-tauri/src/lib.rs
@@ -80,7 +80,6 @@ pub(crate) struct AppState {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![set_remote, submit, reset, query])
         .manage(Mutex::new(AppState::default()))
